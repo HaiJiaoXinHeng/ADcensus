4c62a36789aa98ad4a128bf4e7cbe14bafd17826
ierosodin <ierosodin@gmail.com>
2016-08-02 02:50:18 +0800

    Ver.init

---
+++ build/CMakeFiles/ADCensusBM.dir/DependInfo.cmake
@@ -0,0 +1,51 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/root/StereoVision-ADCensus-master/ADCensusBM/adcensuscv.cpp" "/root/StereoVision-ADCensus-master/ADCensusBM/build/CMakeFiles/ADCensusBM.dir/adcensuscv.cpp.o"
+  "/root/StereoVision-ADCensus-master/ADCensusBM/aggregation.cpp" "/root/StereoVision-ADCensus-master/ADCensusBM/build/CMakeFiles/ADCensusBM.dir/aggregation.cpp.o"
+  "/root/StereoVision-ADCensus-master/ADCensusBM/disparityrefinement.cpp" "/root/StereoVision-ADCensus-master/ADCensusBM/build/CMakeFiles/ADCensusBM.dir/disparityrefinement.cpp.o"
+  "/root/StereoVision-ADCensus-master/ADCensusBM/imageprocessor.cpp" "/root/StereoVision-ADCensus-master/ADCensusBM/build/CMakeFiles/ADCensusBM.dir/imageprocessor.cpp.o"
+  "/root/StereoVision-ADCensus-master/ADCensusBM/main.cpp" "/root/StereoVision-ADCensus-master/ADCensusBM/build/CMakeFiles/ADCensusBM.dir/main.cpp.o"
+  "/root/StereoVision-ADCensus-master/ADCensusBM/scanlineoptimization.cpp" "/root/StereoVision-ADCensus-master/ADCensusBM/build/CMakeFiles/ADCensusBM.dir/scanlineoptimization.cpp.o"
+  "/root/StereoVision-ADCensus-master/ADCensusBM/stereoprocessor.cpp" "/root/StereoVision-ADCensus-master/ADCensusBM/build/CMakeFiles/ADCensusBM.dir/stereoprocessor.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+SET(CMAKE_TARGET_DEFINITIONS
+  "DISABLE_OPENNI2"
+  "DISABLE_PCAP"
+  "DISABLE_PNG"
+  "EIGEN_USE_NEW_STDVECTOR"
+  "EIGEN_YES_I_KNOW_SPARSE_MODULE_IS_NOT_STABLE_YET"
+  "USE_PCL=1"
+  "qh_QHpointer"
+  "vtkFiltersStatistics_AUTOINIT=1(vtkFiltersStatisticsGnuR)"
+  "vtkRenderingCore_AUTOINIT=4(vtkInteractionStyle,vtkRenderingFreeType,vtkRenderingFreeTypeOpenGL,vtkRenderingOpenGL)"
+  "vtkRenderingVolume_AUTOINIT=1(vtkRenderingVolumeOpenGL)"
+  )
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "/usr/include/vtk"
+  "/usr/include/freetype2"
+  "/usr/include/jsoncpp"
+  "/usr/include/libxml2"
+  "/usr/include/python2.7"
+  "/usr/include/pcl-1.7"
+  "/usr/include/eigen3"
+  "/usr/include/ni"
+  "/usr/include/qhull"
+  "/usr/local/include/opencv"
+  "/usr/local/include"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
